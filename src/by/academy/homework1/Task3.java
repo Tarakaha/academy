25a98bac7464cae0546b9778cd47c11aa341621a
Alexandra <wurka@yahoo.com>
2023-11-13 19:08:52 +0300

    Initial commit

--- src/by/academy/homework1/Task3.java
+++ src/by/academy/homework1/Task3.java
@@ -7,9 +7,15 @@ public class Task3 {
    (10 чисел). */
 
     public static void main(String[] args) {
+        int number;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число:");
-        int number = scanner.nextInt();
+        System.out.println("Введите число от 1 до 10:");
+        do{
+            number = scanner.nextInt();
+            if (number < 1 || number > 10){
+                System.out.println("Введите корректное число! (от 1 до 10)");
+            }
+        } while (number < 1 || number > 10);
 
         for (int i = 1; i <= 10; i++) {
             System.out.println(number + " * " + i + " = " + number * i);
