5016b4fe3010a9d8e11fb4af37a897c866c0e15c
Alexandra <wurka@yahoo.com>
2023-12-03 14:28:57 +0300

    Initial commit

--- src/by/academy/lesson9/homework/telegram/task2/Person.java
+++ src/by/academy/lesson9/homework/telegram/task2/Person.java
@@ -28,4 +28,46 @@ public class Person {
     void talk() {
         System.out.println(this.fullName + " говорит.");
     }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        return fullName != null ? fullName.equals(person.fullName) : person.fullName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = fullName != null ? fullName.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "fullName='" + fullName + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
